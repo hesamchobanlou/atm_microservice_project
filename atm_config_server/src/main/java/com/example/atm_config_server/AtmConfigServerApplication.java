467a6b3496b26506cb221ea7384bd50462a6dc36
hesamchobanlou <hesam.chobanlou@gmail.com>
2022-02-17 18:00:18 -0500

    add config server

---
+++ atm_config_server/src/main/java/com/example/atm_config_server/AtmConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.atm_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtmConfigServerApplication.class, args);
+    }
+
+}
